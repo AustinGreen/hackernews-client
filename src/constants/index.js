6e9fddf83a23a9d76164075136f0da08052bc6ac
Austin Green <austinzgreen@gmail.com>
2017-04-08 13:35:37 -0400

    constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,7 +9,7 @@ export const PATH_SEARCH = '/search';
 export const PARAM_SEARCH = 'query=';
 export const PARAM_PAGE = 'page=';
 export const PARAM_HPP = 'hitsPerPage=';
-
+// http://hn.algolia.com/api/v1/search?tags=front_page for front page
 export const SORTS = {
   NONE: list => list,
   TITLE: list => sortBy(list, 'title'),
