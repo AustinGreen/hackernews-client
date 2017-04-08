43df907f35bfa7b8494cf795e02c1936ff276110
Austin Green <austinzgreen@gmail.com>
2017-04-08 17:58:08 -0400

    working on trending tab

--- src/components/Trending/index.js
+++ src/components/Trending/index.js
@@ -1,5 +1,33 @@
 import React, { Component, PropTypes } from 'react';
+import Table from '../Table';
 
-const Trending = () => <h3>Trending</h3>;
+import { PATH_BASE, PATH_SEARCH, PARAM_FRONT_PAGE } from '../../constants';
+
+class Trending extends Component {
+  constructor(props) {
+    super(props);
+
+    // Bind class methods to object instances
+    this.fetchTopstories = this.fetchTopstories.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchTopstories();
+  }
+
+  fetchTopstories() {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_FRONT_PAGE}`)
+      .then(response => response.json())
+      .then(result => console.log(result));
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="section" />
+      </div>
+    );
+  }
+}
 
 export default Trending;
--- src/constants/index.js
+++ src/constants/index.js
@@ -2,13 +2,14 @@ import { sortBy } from 'lodash';
 
 export const DEFAULT_QUERY = 'Tesla';
 export const DEFAULT_PAGE = 0;
-export const DEFAULT_HPP = '100';
+export const DEFAULT_HPP = '15';
 
 export const PATH_BASE = 'https://hn.algolia.com/api/v1';
 export const PATH_SEARCH = '/search';
 export const PARAM_SEARCH = 'query=';
 export const PARAM_PAGE = 'page=';
 export const PARAM_HPP = 'hitsPerPage=';
+export const PARAM_FRONT_PAGE = 'tags=front_page';
 // http://hn.algolia.com/api/v1/search?tags=front_page for front page
 export const SORTS = {
   NONE: list => list,
